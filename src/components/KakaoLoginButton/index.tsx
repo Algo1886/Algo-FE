56560728f1e74a653c760b5a730035c2a19f20df
dewyzxcv <dazzlynnnn@gmail.com>
2025-09-04 21:13:57 +0900

    refactor: 중복 코드 제거

--- src/components/KakaoLoginButton/index.tsx
+++ src/components/KakaoLoginButton/index.tsx
@@ -1,37 +1,11 @@
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { requestKakaoLoginToken } from "@api/auth"
-import { useAuth } from "@contexts/AuthContext"
-
-const KAKAO_REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API
-const REDIRECT_URI = "http://localhost:5173/login/kakao"
+const KAKAO_REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API;
+const REDIRECT_URI = "http://localhost:5173/login/kakao";
 
 const KakaoLoginButton = () => {
-  const navigate = useNavigate()
-  const { setAccessToken, setRefreshToken } = useAuth()
-  const [hasSent, setHasSent] = useState(false)
-
-  const code = new URLSearchParams(window.location.search).get("code")
-
-  useEffect(() => {
-    if (code && !hasSent) {
-      requestKakaoLoginToken(code)
-        .then((data) => {
-          if (data.success && data.data) {
-            setAccessToken(data.data.accessToken)
-            setRefreshToken(data.data.refreshToken) // 여기 추가
-            navigate("/")
-            setHasSent(true)
-          }
-        })
-        .catch((err) => console.error(err))
-    }
-  }, [code, hasSent, setAccessToken, setRefreshToken, navigate])
-
   const goToKakaoAuth = () => {
-    const url = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
-    window.location.href = url
-  }
+    const url = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    window.location.href = url;
+  };
 
   return (
     <button
@@ -49,7 +23,7 @@ const KakaoLoginButton = () => {
       </svg>
       카카오 로그인
     </button>
-  )
-}
+  );
+};
 
-export default KakaoLoginButton
+export default KakaoLoginButton;
