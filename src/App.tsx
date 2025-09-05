28186557e9e385e33673a4f5783ae37a17a3f1e0
dewyzxcv <dazzlynnnn@gmail.com>
2025-09-05 15:15:36 +0900

    style: root css 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import "./App.css";
-
 import { Routes, Route } from "react-router-dom";
 import MainPage from "@pages/MainPage";
 import LoginPage from "@pages/LoginPage";
@@ -23,32 +21,25 @@ function App() {
   return (
     <AuthProvider>
       <Routes>
-      <Route path="/" element={<LayoutWithHeader />}>
-        <Route index element={<MainPage />} /> {/* "/" */}
-          <Route path="/search" element={<SearchResultPage />} />
-          <Route path="/read" element={<ReadSolutionPage />} />
-          <Route path="/create" element={<CreateSolutionPage />} />
-          <Route path="/bookmark" element={<BookmarkPage />} />
+        <Route path="/" element={<LayoutWithHeader />}>
+          <Route index element={<MainPage />} />
+          <Route path="search" element={<SearchResultPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="login/kakao" element={<KakaoLoginPage />} />
           <Route path="login/github" element={<GithubLoginPage />} />
-          <Route path="search-result" element={<SearchResultPage />} />
           <Route path="read/:id" element={<ReadRecordPage />} />
-          <Route path="dashboard" element={<DashboardPage />} />
           <Route element={<LayoutWithSidebar />}>
+            <Route path="dashboard" element={<DashboardPage />} />
+            <Route path="profile" element={<ProfilePage />} />
             <Route path="record/create" element={<CreateRecordPage />} />
             <Route path="record/edit/:id" element={<EditRecordPage />} />
             <Route path="my-ideas" element={<MyIdeasPage />} />
             <Route path="my-records" element={<MyRecordPage />} />
             <Route path="my-drafts" element={<MyDraftsPage />} />
             <Route path="my-bookmarks" element={<MyBookmarksPage />} />
-            <Route path="my-profile" element={<ProfilePage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="/idea" element={<IdeaPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="my-profile" element={<ProfilePage />}/>
           </Route>
         </Route>
-      </Route>
       </Routes>
     </AuthProvider>
   );
