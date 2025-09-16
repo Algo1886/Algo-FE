aa1da69e92e197226f83d7ba8ef8efe82ad6ca16
dewyzxcv <dazzlynnnn@gmail.com>
2025-09-16 14:51:22 +0900

    fix: refreshToken 로직 수정

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,5 @@
 import api from "./axiosInstance"
+import axios from "axios"
 
 export const requestKakaoLoginToken = async (code: string) => {
   const res = await api.post("/auth/kakao-login", { token: code })
@@ -15,7 +16,10 @@ export const requestLogout = async (refreshToken: string) => {
   return res.data
 }
 
+// refresh 요청은 api 대신 순수 axios 사용 (만료된 accessToken 헤더 방지)
 export const requestRefreshToken = async (refreshToken: string) => {
-  const res = await api.post("/auth/refresh", { refreshToken })
+  const res = await axios.post("http://13.209.211.225:8080/api/auth/refresh", { refreshToken }, {
+    headers: { "Content-Type": "application/json" }
+  })
   return res.data
 }
