297f30af4e92e781c733f1c1cebb1b0005655d9a
moonki <moonki0623@naver.com>
2025-09-21 17:51:45 +0900

    fix: 추천 페이지에는 페이지네이션 없음 반영

--- src/pages/MyRecommendPage.tsx
+++ src/pages/MyRecommendPage.tsx
@@ -2,17 +2,16 @@ import { useEffect, useState } from "react";
 import RecordCard from "@components/RecordCard";
 import { fetchUserRecommend } from "@api/user";
 import Button from "@components/Button";
-import type { Record, RecordResponse } from "types/record";
-import Pagination from "@components/Pagination";
+import type { Record } from "types/record";
 
 const MyRecommendPage = () => {
-  const [records, setRecords] = useState<RecordResponse>();
+  const [records, setRecords] = useState<Record[]>();
 
   useEffect(() => {
     const loadRecords = async () => {
       try {
         const res = await fetchUserRecommend();
-        const recordsArray = res.data; // 또는 res.data.records
+        const recordsArray = res.data;
         const mapped: Record[] = recordsArray.map((r: any) => ({
           id: r.id,
           type: r.categories[0],
@@ -21,15 +20,7 @@ const MyRecommendPage = () => {
           author: r.author,
           date: r.createdAt.slice(0, 10),
         }));
-        setRecords({
-          records: mapped,
-          page: res.data.page,
-          size: res.data.size,
-          totalElements: res.data.totalElements,
-          totalPages: res.data.totalPages,
-          first: res.data.first,
-          last: res.data.last,
-        });
+        setRecords(mapped);
       } catch (err) {
         console.error(err);
       }
@@ -39,14 +30,13 @@ const MyRecommendPage = () => {
 
   return (
     <div className="flex flex-col items-center w-full p-5 gap-5">
-      {records?.records?.length === 0 ? (
+      {records?.length === 0 ? (
         <div className="flex flex-col items-center justify-center gap-4 py-20 text-center">
           <div className="w-50 h-50 rounded-full bg-gray-300" />
-          <p className="text-xl font-semibold">아직 기록이 없어요</p>
           <p className="text-gray-600">
-            첫 번째 알고리즘 문제를 풀고
+            모든 추천 기록을
             <br />
-            기록을 남겨보세요!
+            복습하셨어요!
           </p>
           <Button
             onClick={() => {
@@ -57,26 +47,20 @@ const MyRecommendPage = () => {
           </Button>
         </div>
       ) : (
-        <>
-          <div className="grid grid-cols-4 gap-4 w-full max-w-6xl">
-            {records?.records?.map((r) => (
-              <RecordCard
-                key={r.id}
-                id={r.id}
-                problemType={r.type}
-                problemSite={r.site}
-                title={r.title}
-                author={r.author}
-                createdAt={r.date}
-                draft={true}
-              />
-            ))}
-          </div>
-          <Pagination
-            totalPages={records?.totalPages!}
-            currentPage={records?.page!}
-          />
-        </>
+        <div className="grid grid-cols-4 gap-4 w-full max-w-6xl">
+          {records?.map((r) => (
+            <RecordCard
+              key={r.id}
+              id={r.id}
+              problemType={r.type}
+              problemSite={r.site}
+              title={r.title}
+              author={r.author}
+              createdAt={r.date}
+              draft={true}
+            />
+          ))}
+        </div>
       )}
     </div>
   );
