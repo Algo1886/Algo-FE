5113c735f9c27803b09fd9736e6555295c3f89bf
dewyzxcv <dazzlynnnn@gmail.com>
2025-10-02 10:50:01 +0900

    QA: 기록하기 부분 디자인, 로직 수정

--- src/components/Input/InputStep.tsx
+++ src/components/Input/InputStep.tsx
@@ -30,43 +30,23 @@ const InputStep = ({
         <label className="font-medium text-gray-700">
           풀이 과정 {required && <span className="text-blue-500">*</span>}
         </label>
-        <Button
-          theme="white"
-          onClick={() => handleAdd(setSteps, steps, { text: "" })}
-        >
-          <div className="flex gap-3 items-center">
-            <FaPlus size={10} />
-            풀이 과정 추가
-          </div>
-        </Button>
       </div>
-
       {isError && (
         <span className="text-red-500 text-sm">
           풀이 과정을 최소 1개 이상 입력해주세요
         </span>
       )}
-
       {steps.map((c: any, idx: any) => (
         <div
           key={idx}
-          className={`border rounded-lg p-3 space-y-2 relative ${
-            isError && !c.text.trim() ? "border-red-500" : "border-gray-200"
+          className={`rounded-lg p-3 space-y-2 relative ${
+            isError && !c.text.trim() ? "border border-red-500" : ""
           }`}
         >
-          <div className="flex justify-between">
-            <label className="font-medium text-gray-700">풀이 {idx + 1}</label>
-            <div className="flex gap-2 items-center">
-              {idx > 0 && (
-                <button
-                  className="ml-auto text-gray-500 hover:text-gray-700"
-                  onClick={() => handleRemove(setSteps, steps, idx)}
-                >
-                  <FaTrash />
-                </button>
-              )}
-            </div>
-          </div>
+          <div className="flex flex-row gap-2">
+          <span className="inline-flex flex-none h-7 w-7 items-center justify-center rounded-full bg-black text-white text-sm">
+            {idx + 1}
+          </span>
           <InputBox
             value={c.text}
             setValue={(e) => {
@@ -75,9 +55,39 @@ const InputStep = ({
               setSteps(newSteps)
             }}
             placeholder="풀이 과정을 입력하세요"
+            isStep={true}
           />
+          <div className="ml-auto w-6 flex justify-center">
+            {idx > 0 ? (
+              <button
+                className="text-gray-500 hover:text-gray-700"
+                onClick={() => handleRemove(setSteps, steps, idx)}
+              >
+                <FaTrash />
+              </button>
+            ) : (
+              <span className="opacity-0"><FaTrash /></span>
+            )}
+          </div>
+          </div>
+          
         </div>
+
       ))}
+      {steps.length < 10 && (
+        <div className="self-center">
+          <Button
+            theme="white"
+            className="w-auto"
+            onClick={() => handleAdd(setSteps, steps, { text: "" })}
+          >
+            <div className="flex gap-3 items-center">
+              <FaPlus size={10} />
+              풀이 과정 추가
+            </div>
+          </Button>
+        </div>
+      )}
     </div>
   )
 }
